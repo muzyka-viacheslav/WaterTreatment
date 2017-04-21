e92e9ad7176523edbccc402c85a6702b80809fe3
Viacheslav Muzyka <Viacheslav Muzyka>
2017-04-21 18:25:50 +0300

    add authentication

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -16,8 +16,43 @@ export function NavbarDirective() {
 }
 
 class NavbarController {
-  constructor ($state) {
+  constructor ($state, $scope, api) {
     'ngInject';
     this.$state = $state;
+    this.$scope = $scope;
+    this.api = api;
+    this.user = {};
+    this.loginError = false;
+    var tryLogin = localStorage.getItem('waterTreatmentLogin');
+    var tryPassword = localStorage.getItem('waterTreatmentPassword');
+    if (tryLogin && tryPassword) {
+      this.login();
+    }
+  }
+
+  login() {
+    this.api
+      .get('login', {
+        login: this.user.login,
+        password: this.user.password
+      })
+      .then(response => {
+        this.user = response.data;
+        this.$scope.$root.authenticated = true;
+        $("#loginModal").modal('hide');
+        this.loginError = false;
+        localStorage.setItem('waterTreatmentLogin', this.user.login);
+        localStorage.setItem('waterTreatmentPassword', this.user.password);
+      }, () => {
+        this.$scope.$root.authenticated = false;
+        this.user = {};
+        this.loginError = true;
+      });
+  }
+
+  logout() {
+    this.$scope.$root.authenticated = false;
+    this.user = {};
+    this.$state.go('home');
   }
 }
