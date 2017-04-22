6b1c54f656a943d70b26b026a2da51f82ce163a5
Viacheslav Muzyka <Viacheslav Muzyka>
2017-04-22 17:04:02 +0300

    add authorization suka ne pizdi

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -16,8 +16,44 @@ export function NavbarDirective() {
 }
 
 class NavbarController {
-  constructor ($state) {
+  constructor ($state, $scope, api) {
     'ngInject';
     this.$state = $state;
+    this.$scope = $scope;
+    this.api = api;
+    this.user = {};
+    this.loginError = false;
+    this.user.login = localStorage.getItem('waterTreatmentLogin');
+    this.user.password = localStorage.getItem('waterTreatmentPassword');
+    if (this.user.login && this.user.password) {
+      this.login();
+    }
+  }
+  login() {
+    this.api
+      .get('login', {
+        login: this.user.login,
+        password: this.user.password
+      })
+      .then(response => {
+        this.user = response.data;
+        this.$scope.$root.authenticated = true;
+        $("#loginModal").modal('hide');
+        this.loginError = false;
+        localStorage.setItem('waterTreatmentLogin', this.user.login);
+        localStorage.setItem('waterTreatmentPassword', this.user.password);
+      }, () => {
+        this.$scope.$root.authenticated = false;
+        this.user = {};
+        this.loginError = true;
+      });
+  }
+
+  logout() {
+    this.$scope.$root.authenticated = false;
+    this.user = {};
+    this.$state.go('home');
+    localStorage.removeItem('waterTreatmentLogin');
+    localStorage.removeItem('waterTreatmentPassword');
   }
 }
